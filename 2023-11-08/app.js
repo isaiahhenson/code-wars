91db8adfde147ee2d09c5434b641214661e1c503
Isaiah Henson <Devpuddles@gmail.com>
2023-11-08 07:29:21 -0500

    the actual code

---
+++ 2023-11-08/app.js
@@ -0,0 +1,11 @@
+//PREP
+//I will need to call this function
+//The function needs a return keyword to return a value
+//by calling the result is then available
+// return a*b within the function
+//return a*b 
+
+function multiply(a, b){
+    return a * b
+  }
+  
