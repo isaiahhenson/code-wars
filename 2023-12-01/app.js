af81fdb1010297c26c927ddc3d46ededc6f38529
Isaiah Henson <Devpuddles@gmail.com>
2023-12-01 15:21:43 -0500

    completed kata for 2023-12-01

---
+++ 2023-12-01/app.js
@@ -0,0 +1,16 @@
+//An interger will be entered into the function
+//the function will convert it into a string
+// 123  --> "123"
+// 5 is entered into the parameter and "5" is then return from parameter
+
+
+
+function numberToString(num) {
+   let str = num.toString()
+    return str
+
+}
+numberToString(67)
+
+//Kata Complete
+
