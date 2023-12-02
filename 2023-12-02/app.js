0d04dcabfbbb337e50af4758e1f38eedb2da3463
Isaiah Henson <Devpuddles@gmail.com>
2023-12-02 11:15:38 -0500

    completed challenge for 12-02-23 returning false

---
+++ 2023-12-02/app.js
@@ -0,0 +1,12 @@
+//no parameter
+//return false without falsey vlaue
+//function --> return false
+//function will run and return false 
+
+function ifChuckSaysSo(){
+    return !true
+}
+
+console.log(ifChuckSaysSo())
+
+//kata cooplete
