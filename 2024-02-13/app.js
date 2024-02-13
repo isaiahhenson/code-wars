e8c960027a5abf15f34d9f53840cd4e44b37faa5
Isaiah Henson <Devpuddles@gmail.com>
2024-02-13 15:12:39 -0500

    completed third kata for today

--- 2024-02-13/app.js
+++ 2024-02-13/app.js
@@ -16,10 +16,27 @@
 //assert.strictEqual(squareSum([]), 0);
 //loop through array squaring each number then add them. I will use the reduce method
 
-function squareSum(numbers) {
-  let sum = numbers.reduce((acc, current) => {
-    return acc + current * current;
-  }, 0);
-  console.log(sum);
+// function squareSum(numbers) {
+//   let sum = numbers.reduce((acc, current) => {
+//     return acc + current * current;
+//   }, 0);
+//   console.log(sum);
+// }
+// squareSum([1, 2]);
+
+//an array will be entered into function
+//the function will return a integer number which is the smallest
+//[34, 15, 88, 2] --> 2
+//Initialize a variable, set it to the first element in the array. them compare the remaining elements to that variable. If they are less then I will reassign the array.
+
+function findSmallestInt(args) {
+  let smallNum = args[0];
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] < smallNum) {
+      smallNum = args[i];
+    }
+  }
+  return smallNum;
 }
-squareSum([1, 2]);
+
+findSmallestInt([78, 56, 232, 12, 0]);
