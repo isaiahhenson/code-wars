b80b9b5c843f58a61102c64090cea408aa705533
Isaiah Henson <Devpuddles@gmail.com>
2024-02-13 14:11:34 -0500

    completed coding challenge for FEB 13th

---
+++ 2024-02-13/app.js
@@ -0,0 +1,9 @@
+//this will take in a string
+//to return the string with first & last characters removed
+//'apple' --> 'ppl'
+//I will use the slice method on the string
+function removeChar(str) {
+  let newStr = str.slice(1, -1);
+}
+
+removeChar('apple');
