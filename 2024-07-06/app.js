2af43a2c2e1743ea29face59b7f49031290df705
Isaiah Henson <Devpuddles@gmail.com>
2024-07-06 21:52:06 -0400

    completed another challenge for codewars

--- 2024-07-06/app.js
+++ 2024-07-06/app.js
@@ -6,10 +6,43 @@
 //Test.assertEquals(stringToNumber("-7"),  -7)
 //This function will intake string '567' --> return 567 not string
 
-const stringToNumber = function (str) {
-  let newStr = +str;
-  console.log(typeof newStr);
-  return newStr;
-};
+// const stringToNumber = function (str) {
+//   let newStr = +str;
+//   return newStr;
+// };
 
-stringToNumber('567');
+// stringToNumber('567');
+
+//Tried to recreat using a IIFE
+
+// (function (num = '561') {
+//   console.log(typeof num);
+//   let newNum = +num;
+//   console.log(typeof newNum);
+//   return newNum;
+// })();
+
+//This function will receive a string value
+//Number value will be returned, amount of vowels string.
+//the word count has 2 vowel numbers. return 2
+// if letters are present add it too word count.
+
+function getCount(str) {
+  let strCount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (
+      str[i] === 'a' ||
+      str[i] === 'e' ||
+      str[i] === 'i' ||
+      str[i] === 'o' ||
+      str[i] === 'u'
+    ) {
+      console.log(str[i]);
+      strCount++;
+    }
+  }
+  console.log(strCount);
+  return 0;
+}
+
+getCount('apple');
